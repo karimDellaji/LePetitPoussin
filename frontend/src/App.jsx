b1cf0c41b3f63177cf909efd1e8b032250e2c023
Karim Dellaji <delleji.karim@gmail.com>
2026-01-26 00:39:55 +0100

    Add: AdminDashboard with Fresh Emerald design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// On change "pages" par "components" ici :
-import Login from './components/Login'; 
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard'; // Ajoute cette ligne
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* La page par défaut affiche le composant Login */}
           <Route path="/" element={<Login />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} /> {/* Et cette ligne */}
         </Routes>
       </div>
     </Router>
