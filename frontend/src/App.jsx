ba6169777e0fa079b09c1cb2230954621993c4fd
Karim Dellaji <delleji.karim@gmail.com>
2026-01-26 00:28:33 +0100

    Fix: Update import path to components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-
-// Importe tes dashboards ici (assure-toi que les fichiers existent)
-// import AdminDashboard from './pages/AdminDashboard'; 
+// On change "pages" par "components" ici :
+import Login from './components/Login'; 
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* La page par défaut est le Login */}
+          {/* La page par défaut affiche le composant Login */}
           <Route path="/" element={<Login />} />
-          
-          {/* On ajoute les autres routes ici plus tard */}
-          {/* <Route path="/admin-dashboard" element={<AdminDashboard />} /> */}
         </Routes>
       </div>
     </Router>
